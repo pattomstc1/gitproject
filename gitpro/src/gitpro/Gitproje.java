52481967564fa8f5c267f1581da560c1f6076ae3
DEVU <devisklm89@gmail.com>
2023-02-08 21:53:24 +0530

    devi commit the project

---
+++ gitpro/src/gitpro/Gitproje.java
@@ -0,0 +1,39 @@
+package gitpro;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Gitproje {
+
+	public static void main(String[] args)
+	//login
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","C:\\Devi Work Document\\automation\\selenium\\geckodriver-v0.32.0-win32\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://localhost/orangehrm/orangehrm-5.3/web/index.php/auth/login");
+ driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		driver.findElement(By.name("username")).sendKeys("admin"); 	    
+		driver.findElement(By.name("password")).sendKeys("Pass@12345");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	
+//Add employee
+
+driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")).click(); 
+driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[3]/a")).click(); 
+driver.findElement(By.name("firstName")).sendKeys("Devi");
+driver.findElement(By.name("lastName")).sendKeys("Sheela");
+driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[2]/div/label/span")).click();
+driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[3]/div/div[1]/div/div[2]/input")).sendKeys("Devika");
+driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[4]/div/div[1]/div/div[2]/input")).sendKeys("Pass@1234");
+driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[4]/div/div[2]/div/div[2]/input")).sendKeys("Pass@1234");
+driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]")).click();
+
+}
+}
